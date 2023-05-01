f966c217e207356f9980cf9b43cdeb674067c469
Colejxs <cspencer24@hanoverstudents.org>
2023-05-01 11:56:29 -0400

    Milestone1

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styles.css?v=<?php echo rand(); ?>">
+<style>
+</style>
+<script src=""></script>
+<body>
+    <section>
+        <header>
+            <a href="#"><img src="bag.png" class="logo"></a>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Shopping Cart</a></li>
+                <li><a href="#">Sign In</a></li>
+                <li><a href="#">Sign Up</a></li>
+            </ul>
+        </header>
+        <div class="content">
+            <div class="textBox">
+                <h2>It's not just <span>Bag</span></h2>
+                <p>The Cool Green Bag is an oppritunity to give back to your comunuty.
+                </p>
+                <a href="#">Cancel This Week</a>
+            </div>
+            <div class="imgBox">
+                <img src="foodbag.png" class="starbucks">
+            </div>
+        </div>
+        <ul class="sci">
+            <li><a href="#"><img src="facebook.png"></a></li>
+            <li><a href="#"><img src="twitter.png"></a></li>
+            <li><a href="#"><img src="instagram.png"></a></li>
+            
+        </ul>
+    </section>
+</body>
+</html>
